7ad792085bc8dfd9824287a77e343691f2b3fbf8
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2021-11-24 23:56:58 +0600

    Update intersec.php

--- intersec.php
+++ intersec.php
@@ -1,9 +1,9 @@
 <?php
 
 $a1 = array(" a " => "red", " b " => "blue", " c " => "white",);
-$a2 = array(" a " => "red", " d " => "green", " e " => "lime",);
+$a2 = array(" a " => "red", " b " => "green", " e " => "lime",);
 
-     $new = array_intersect_key($a1, $a2);
+     $new = array_intersect_assoc($a1, $a2);
  echo "<pre>";
  print_r($new);
  echo "</pre>";
