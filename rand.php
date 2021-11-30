a180878ee92075abde2e2f60be8eb75ebc3132a9
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2021-11-30 08:03:32 +0600

    Create rand.php

---
+++ rand.php
@@ -0,0 +1,14 @@
+<?php
+
+$group = [34, 35, 37, 38];
+
+
+$new = array_rand($group, 2);
+echo "<pre>";
+print_r($new);
+echo "</pre>";
+
+echo $group[$new [0]]. "<br>"; 
+echo $group[$new [1]]; 
+
+?>
